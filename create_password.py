8d39f6e988fb8a46eac1c368e5f0dedb27129248
austinguin0709 <austinguin0709@gmail.com>
2025-03-03 21:12:53 -0500

    Updated functionality to save password to file.

--- create_password.py
+++ create_password.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import string
 import random
 import argparse
@@ -10,7 +12,8 @@ special_chars = string.punctuation
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Generate a password and save to a txt file on desktop."
+        description="""Generate a password and save to a txt file on desktop.
+                    options are l (lowercase), u (uppercase), n (numbers), s (special characters), or all."""
     )
     parser.add_argument(
         "options_or_length",
@@ -39,6 +42,32 @@ def main():
 
     password = create_password_random(options, length)
     print(f"Password: {password}\nOptions: {options}\nLength: {length}")
+
+    save_to_file = input("Save this password to file? [Y/N]: ")
+
+    if save_to_file.lower() == "n":
+        sys.exit("Password not saved to file...")
+    elif save_to_file.lower() == "y":
+        try:
+            path = os.path.join(os.path.join(
+                os.environ["USERPROFILE"]), "Desktop")
+            name = "password_vault.txt"
+
+            use_for_password = input(
+                "The name or thing that the password is for: ")
+
+            file = open(os.path.join(path, name), "a")
+            file.write(f"{use_for_password}: {password}\n")
+            file.close()
+
+            file = open(os.path.join(path, name), "r")
+            print(file.read())
+            file.close()
+
+        except KeyError:
+            raise KeyError("Error occurred trying to access desktop path.")
+    else:
+        sys.exit("Invalid response to question... Exiting program")
 
 
 def create_password_random(options="all", length=8):
