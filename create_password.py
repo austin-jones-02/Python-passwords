736049959ba7005e7b05e1695cee72d1cf16f289
austinguin0709 <austinguin0709@gmail.com>
2025-03-03 19:02:41 -0500

    Went with randomization method after adding to character pool. Justification is after, testing results showed the passwords generated were basically the same. Because of this the before generation style randomized 3 times and thus was thought to have a longer run time. After doing some testing and averaging results it was found to take a longer runtime 50% of the time compared to just randomizing once

--- create_password.py
+++ create_password.py
@@ -6,12 +6,11 @@ Note:
         - Lowercase
         - Uppercase
         - Numbers
-        - Randomized symbols aka special chars    
+        - Randomized symbols aka special chars
 """
 
 import string
 import random
-import time
 
 lower_chars = string.ascii_lowercase
 upper_chars = string.ascii_uppercase
@@ -19,39 +18,7 @@ nums = string.digits
 special_chars = string.punctuation
 
 
-def create_password_random_before(options="all", length=8):
-    start = time.perf_counter()
-
-    # Initialize character pool (What the password will pick from for creation)
-    char_pool = ""
-
-    if "l" in options or options.lower() == "all":
-        char_pool += "".join([random.choice(lower_chars)
-                             for _ in range(len(lower_chars) + 1)])
-    if "u" in options or options.lower() == "all":
-        char_pool += "".join([random.choice(upper_chars)
-                             for _ in range(len(upper_chars) + 1)])
-    if "n" in options or options.lower() == "all":
-        char_pool += "".join([random.choice(nums)
-                             for _ in range(len(nums) + 1)])
-    if "s" in options or options.lower() == "all":
-        char_pool += "".join([random.choice(special_chars)
-                             for _ in range(len(special_chars) + 1)])
-
-    char_pool = "".join([random.choice(char_pool)
-                        for _ in range(len(char_pool) + 1)])
-    print(f"Character pool used for password: {char_pool}")
-
-    password = "".join([random.choice(char_pool) for _ in range(length + 1)])
-
-    end = time.perf_counter()
-    print(f"Random generation BEFORE runtime: {end - start:.6f}")
-    return password
-
-
-def create_password_random_after(options="all", length=8):
-    start = time.perf_counter()
-
+def create_password_random(options="all", length=8):
     # Initialize character pool (What the password will pick from for creation)
     char_pool = ""
 
@@ -64,16 +31,13 @@ def create_password_random_after(options="all", length=8):
     if "s" in options or options.lower() == "all":
         char_pool += special_chars
 
-    print(f"Character pool used for password: {char_pool}")
     password = "".join([random.choice(char_pool) for _ in range(length + 1)])
 
-    end = time.perf_counter()
-    print(f"Random generation AFTER runtime: {end - start:.6f}")
     return password
 
 
 if __name__ == '__main__':
-    print(
-        f"\nPassword with lower and upper and nums: {create_password_random_before("luns", 10)}")
-    print(
-        f"\nPassword with lower and upper and nums: {create_password_random_after("luns", 10)}")
+    print(create_password_random('l', 10))
+    print(create_password_random('lu', 10))
+    print(create_password_random('lun', 10))
+    print(create_password_random('all', 10))  # also could do "luns"
